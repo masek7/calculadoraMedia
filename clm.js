d2359a00c16ef6c3db27f9e73847986179a6e62d
Guilherme Oliveira <74804474+masek7@users.noreply.github.com>
2021-11-09 12:01:22 -0300

    Add files via upload

--- clm.js
+++ clm.js
@@ -1,19 +1,19 @@
 
 function calcular(){
 
-    var notaCorte = document.getElementById('corte');
+    let notaCorte = document.getElementById('corte');
 
-    var nota1 = document.getElementById('Nota1');
-    var nota2 = document.getElementById('Nota2');
-    var nota3 = document.getElementById('Nota3');
-    var nota4 = document.getElementById('Nota4');
+    let nota1 = document.getElementById('Nota1');
+    let nota2 = document.getElementById('Nota2');
+    let nota3 = document.getElementById('Nota3');
+    let nota4 = document.getElementById('Nota4');
 
-    var notaFinal = document.getElementById("NotaFinal");
+    let notaFinal = document.getElementById("NotaFinal");
 
 
     const media = Number(nota1.value) + Number(nota2.value) + Number(nota3.value) + Number(nota4.value);
 
-    var mediaFinal = Number(media /4).toFixed(2);
+    let mediaFinal = Number(media /4).toFixed(2);
 
     notaCorte = Number(notaCorte.value);
 
